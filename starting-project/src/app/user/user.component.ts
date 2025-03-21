5e22588b0950565d6ef2019f3597aa8c138179d7
Sasi <sasi@test.com>
2025-03-21 17:18:15 +0530

    replacing signals with property binding

--- starting-project/src/app/user/user.component.ts
+++ starting-project/src/app/user/user.component.ts
@@ -1,7 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
-
-
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
@@ -10,18 +8,21 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  // selectedUser = DUMMY_USERS[randomIndex];
 
-  get imagePath(){
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  /* get imagePath(){
     return 'assets/users/' + this.selectedUser.avatar;
-  }
+  } */
 
-  onSelectUser(){
+  onSelectUser() {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    // this.selectedUser = DUMMY_USERS[randomIndex];
+
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
- 
