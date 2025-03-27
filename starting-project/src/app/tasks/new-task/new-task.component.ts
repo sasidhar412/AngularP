2ae6315b503f4de5a21981f0cbe9a5c95726269a
Sasi <sasi@test.com>
2025-03-27 15:20:41 +0530

    two way binding

--- starting-project/src/app/tasks/new-task/new-task.component.ts
+++ starting-project/src/app/tasks/new-task/new-task.component.ts
@@ -1,16 +1,20 @@
-import { Component, EventEmitter, Output, output } from '@angular/core';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
-  styleUrl: './new-task.component.css'
+  styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
   @Output() isCancelled = new EventEmitter<void>(); // using void, if we are not emitting specific type
+  enteredTitle = '';
+  enteredSummary = signal(''); // using signal for a two way binding
+  enteredDate = '';
 
-  toCancelled(){
-    this.isCancelled.emit();  // emitting void
+  toCancelled() {
+    this.isCancelled.emit(); // emitting void
   }
 }
